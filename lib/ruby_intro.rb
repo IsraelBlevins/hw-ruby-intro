ecb799f60d489fad300e6eca79b4a5aee42cd604
Israel Blevins <israel.blevins@tamu.edu>
2020-01-31 05:07:46 +0000

    Finished Homework One

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,92 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  sum = 0
+  arr.each { |n| sum += n}
+  return sum
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  if(arr.size() == 0)
+    return 0
+  end
+  if(arr.size() == 1)
+    return arr[0]
+  end
+  
+  sum = 0
+  maxs = arr.max(2)
+  sum = maxs[0] + maxs[1]
+  return sum
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  if(arr.size() <= 1)
+    return false
+  end
+  
+  for i in arr 
+    for j in arr
+      if(i+j == n and i != j)
+        return true
+      end
+    end
+  end
+  
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, ".concat(name)
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if(s.size == 0)
+    return false
+  end
+  
+  return /[^[:punct:]aeiou0-9]/ix.match?(s[0])
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if(/[[:alpha:][:punct:][:space:]]/i.match?(s) == true or s.size == 0)
+    return false
+  end
+  
+  return s.to_i(base=2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(i, p)
+    raise ArgumentError, 'ISBN can not be an empty string' unless i.size > 0
+    raise ArgumentError, 'PRICE can not be negative or zero' unless p > 0
+    
+    @isbn = i
+    @price = p
+  end
+  
+  def isbn
+    return @isbn
+  end
+  
+  def price
+    return @price
+  end
+  
+  def isbn=(i)
+    @isbn = i
+  end
+  
+  def price=(p)
+    @price = p
+  end
+  
+  def price_as_string
+    return "$".concat('%.2f' % @price)
+  end
 end
